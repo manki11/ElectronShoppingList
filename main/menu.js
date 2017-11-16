65d460cb170a0fc367862bb66f12974c53c306ba
Mankirat Singh <mankirat1998@gmail.com>
2017-11-16 19:21:27 +0530

    feat: developer tools and mac compatibility added in menu

--- main/menu.js
+++ main/menu.js
@@ -2,10 +2,10 @@ const electron = require('electron');
 const {app} = electron;
 //Create menu template
 
-module.exports = function(fns) {
+module.exports = function (fns) {
     let mainMenuTemplate = [
         {
-            label: 'Electron',
+            label: 'File',
             submenu: [
                 {
                     label: 'About',
@@ -32,5 +32,31 @@ module.exports = function(fns) {
         }
     ];
 
+    //Add developer tools item if not in production
+    if(process.env.NODE_ENV !== 'production') {
+        mainMenuTemplate.push({
+            label: 'Developer Tools',
+            submenu: [
+                {
+                    label: 'Toggle Dev Tools',
+                    click: (item, foccusedWindow) => {
+                        foccusedWindow.toggleDevTools();
+                    },
+                    accelerator: process.platform === 'darwin' ? 'Command+I' : 'Ctrl+I'
+                }, {
+                    type: 'separator'
+                },  {
+                    role: 'reload'
+                }
+            ]
+        })
+    }
+
+    //If mac add empty object to menu
+    if (process.platform === 'darwin') {
+        mainMenuTemplate.unshift({});
+    }
+
     return mainMenuTemplate;
+
 };
